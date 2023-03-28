4709ed8e119ce38a617bd4f536b8b0561156dade
Sundus <sundusaden105@gmail.com>
2023-03-28 15:37:42 +0300

    code refactored NavBar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import Navbar from './components/Navbar';
+import React from 'react';
+import NavBar from './components/NavBar';
 import './App.css';
 
 function App() {
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,13 @@
 import {NavLink} from 'react-router-dom'
+
 const Nav = ()=> {
     return (
     <nav className="nav1">
         <NavLink to="/loginPage" style={{paddingRight: "15px",}}>Login</NavLink>
         <NavLink to="/Portfolio" style={{paddingRight: "15px",}}>Portfolio</NavLink>
         <NavLink to="/HomePage" style={{paddingRight: "15px",}}>Home</NavLink>
+
     </nav>
     );
-  }
+  };
 export default Nav;
