100e19ab7c1d9de2875d376fa48044510d4d1151
Sundus <sundusaden105@gmail.com>
2023-03-29 10:25:36 +0300

    updated

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
-import {NavLink} from 'react-router-dom'
-const Nav = ()=> {
-    return (
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+  return (
     <nav className="nav1">
-        <NavLink to="/HomePage" style={{paddingRight: "15px",}}>Home</NavLink>
-        {Nav}
+      <Link to="/HomePage" style={{ paddingRight: "15px" }}>Home</Link>
     </nav>
-    );
-  }
+  );
+}
+
 export default Nav;
